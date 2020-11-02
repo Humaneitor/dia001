50b00b99a0c5cfbc6ae7a9f6b5042b4d225317a6
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-11-02 19:40:43 +0100

    Mi primer oabjeto ya está creado

--- perro.py
+++ perro.py
@@ -5,4 +5,12 @@ class Perro():
         self.peso = p
     
     def ladrar(self):
-        print("Guau, Guau")
+        if self.peso >= 8:
+            print("Guau, Guau", self.nombre)
+        else:
+            print("guau, guau", self.nombre)
+            
+    def __str__(self):
+        return "Perro{}, e: {} años, p: {} kilos".format(self.nombre, self.edad, self.peso)
+    
+            
